4e80e5ebdab96979c84d59ca9cd2411affb9a592
Huw Wynn-Jones <huw@huwwynnjones.com>
2020-12-01 22:04:00 +0000

    Added a test tthe covered the example input in a different order. Implemented a working solution.

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,5 +1,12 @@
-fn find_items_that_sum_2020(input: &[u32]) -> (u32, u32) {
-    (1721, 299)
+fn find_items_that_sum_2020(input: &[u32]) -> Option<(u32, u32)> {
+    for i in input {
+        for j in input {
+            if i + j == 2020 {
+                return Some((*i, *j));
+            }
+        }
+    }
+    None
 }
 
 #[cfg(test)]
@@ -9,6 +16,12 @@ mod test {
     #[test]
     fn test_find_items_that_sum_2020() {
         let input = [1721, 979, 366, 299, 675, 1456];
-        assert_eq!(find_items_that_sum_2020(&input), (1721, 299))
+        assert_eq!(find_items_that_sum_2020(&input), Some((1721, 299)))
+    }
+
+    #[test]
+    fn test_find_items_that_sum_2020_alternate_order() {
+        let input = [979, 675, 1721, 366, 1456, 299, 1456];
+        assert_eq!(find_items_that_sum_2020(&input), Some((1721, 299)))
     }
 }
