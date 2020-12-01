9952247dd1bea7dc09991f676a8d57373fa8b3ee
Huw Wynn-Jones <huw@huwwynnjones.com>
2020-12-01 22:53:40 +0000

    Rename function.

--- src/day_1.rs
+++ src/day_1.rs
@@ -4,7 +4,7 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-pub fn find_items_that_sum_2020(input: &[u32]) -> Option<(u32, u32)> {
+pub fn find_two_items_that_sum_2020(input: &[u32]) -> Option<(u32, u32)> {
     for i in input {
         for j in input {
             if i + j == 2020 {
@@ -54,13 +54,13 @@ mod test {
     #[test]
     fn test_find_items_that_sum_2020() {
         let input = [1721, 979, 366, 299, 675, 1456];
-        assert_eq!(find_items_that_sum_2020(&input), Some((1721, 299)))
+        assert_eq!(find_two_items_that_sum_2020(&input), Some((1721, 299)))
     }
 
     #[test]
     fn test_find_items_that_sum_2020_alternate_order() {
         let input = [979, 675, 1721, 366, 1456, 299, 1456];
-        assert_eq!(find_items_that_sum_2020(&input), Some((1721, 299)))
+        assert_eq!(find_two_items_that_sum_2020(&input), Some((1721, 299)))
     }
 
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 mod day_1;
 
-use day_1::{find_items_that_sum_2020, find_three_items_that_sum_2020, load_input_file};
+use day_1::{find_three_items_that_sum_2020, find_two_items_that_sum_2020, load_input_file};
 
 fn main() {
     println!("Day 1 part 1");
     let day_1_input = load_input_file("day_1.txt").expect("Missing input file");
-    if let Some(numbers) = find_items_that_sum_2020(&day_1_input) {
+    if let Some(numbers) = find_two_items_that_sum_2020(&day_1_input) {
         let answer = numbers.0 * numbers.1;
         println!("Answer is {}", answer)
     };
