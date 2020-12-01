123a084c66bd18b26c85dd7f3acfe2b153d696e0
Huw Wynn-Jones <huw@huwwynnjones.com>
2020-12-01 22:47:54 +0000

    Added test for finding three items, and solution

--- src/day_1.rs
+++ src/day_1.rs
@@ -15,6 +15,23 @@ pub fn find_items_that_sum_2020(input: &[u32]) -> Option<(u32, u32)> {
     None
 }
 
+pub fn find_three_items_that_sum_2020(input: &[u32]) -> Option<Vec<u32>> {
+    for i in input {
+        for j in input {
+            for k in input {
+                if i + j + k == 2020 {
+                    let mut numbers = Vec::new();
+                    numbers.push(*i);
+                    numbers.push(*j);
+                    numbers.push(*k);
+                    return Some(numbers);
+                }
+            }
+        }
+    }
+    None
+}
+
 pub fn load_input_file(file_name: &str) -> io::Result<Vec<u32>> {
     let input = File::open(file_name)?;
     let reader = BufReader::new(input);
@@ -50,5 +67,14 @@ mod test {
     fn test_load_input_file() {
         let input = load_input_file("day_1_test.txt").expect("Unable to load the file");
         assert_eq!(input, [1472, 1757, 1404])
+    }
+
+    #[test]
+    fn test_find_three_items_that_sum_2020() {
+        let input = [1721, 979, 366, 299, 675, 1456];
+        assert_eq!(
+            find_three_items_that_sum_2020(&input),
+            Some(Vec::from([979, 366, 675]))
+        )
     }
 }
