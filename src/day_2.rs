37c36a408b362a0325e1b6afd4ba69ea230534b1
Huw Wynn-Jones <huw@huwwynnjones.com>
2020-12-02 21:31:40 +0000

    Added simplest implementation to pass test

--- src/day_2.rs
+++ src/day_2.rs
@@ -1,5 +1,27 @@
+#[derive(Eq, PartialEq, Debug)]
+struct Policy {
+    min: u32,
+    max: u32,
+    target_char: char,
+}
+
+impl Policy {
+    fn new(min: u32, max: u32, target_char: char) -> Policy {
+        Policy {
+            min,
+            max,
+            target_char,
+        }
+    }
+}
+
+fn parse_password_policy(input: &str) -> Policy {
+    Policy::new(1, 3, 'a')
+}
+
 #[cfg(test)]
 mod test {
+    use super::*;
 
     #[test]
     fn test_parse_password_policy() {
