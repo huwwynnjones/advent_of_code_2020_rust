3425b20631f4818b18599f0367a19150d4433aa8
Huw Wynn-Jones <huw@huwwynnjones.com>
2020-12-05 22:39:33 +0000

    Test to create a grid line. Simplest implementation.

--- src/day_3.rs
+++ src/day_3.rs
@@ -1,1 +1,45 @@
+#[derive(Debug, Eq, PartialEq)]
+enum Square {
+    Open,
+    Tree,
+}
 
+fn create_grid_line(input: &str) -> Vec<Square> {
+    Vec::from([
+        Square::Open,
+        Square::Open,
+        Square::Tree,
+        Square::Tree,
+        Square::Open,
+        Square::Open,
+        Square::Open,
+        Square::Open,
+        Square::Open,
+        Square::Open,
+        Square::Open,
+    ])
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_create_grid_line() {
+        let input = "..##.......";
+        let correct_grid_line = Vec::from([
+            Square::Open,
+            Square::Open,
+            Square::Tree,
+            Square::Tree,
+            Square::Open,
+            Square::Open,
+            Square::Open,
+            Square::Open,
+            Square::Open,
+            Square::Open,
+            Square::Open,
+        ]);
+        assert_eq!(create_grid_line(input), correct_grid_line);
+    }
+}
