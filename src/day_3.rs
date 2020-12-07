e1ab1c68e6fe3afa74ed5873c6dd68cd17c6ce36
Huw Wynn-Jones <huw@huwwynnjones.com>
2020-12-07 21:09:28 +0000

    Test for find positions added and simplest implementation.

--- src/day_3.rs
+++ src/day_3.rs
@@ -19,6 +19,13 @@ fn move_tobogan(start_position: (usize, usize), slope: (usize, usize)) -> (usize
     (start_position.0 + slope.0, start_position.1 + slope.1)
 }
 
+fn positions_used_to_reach_bottom(
+    grid: &[Vec<Square>],
+    slope: (usize, usize),
+) -> Vec<(usize, usize)> {
+    Vec::from([(3, 1), (6, 2), (9, 4)])
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -81,6 +88,24 @@ mod test {
         let start_position = (0, 0);
         let slope = (3, 1);
         let final_position = (3, 1);
-        assert_eq!(move_tobogan((0, 0), (3, 1)), (3, 1))
+        assert_eq!(move_tobogan(start_position, slope), final_position)
+    }
+
+    #[test]
+    fn test_positions_used_to_reach_bottom() {
+        let input = Vec::from([
+            "..##.......".to_string(),
+            "#...#...#..".to_string(),
+            ".#....#..#.".to_string(),
+            "..#.#...#.#".to_string(),
+        ]);
+        let grid = create_grid(&input);
+        let slope = (3, 1);
+
+        let correct_positions = Vec::from([(3, 1), (6, 2), (9, 4)]);
+        assert_eq!(
+            positions_used_to_reach_bottom(&grid, slope),
+            correct_positions
+        )
     }
 }
