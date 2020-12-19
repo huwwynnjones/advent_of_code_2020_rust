20b75100a9f62e633ff0f3a9716e9b649a666631
Huw Wynn-Jones <huw@huwwynnjones.com>
2020-12-19 23:57:33 +0000

    Test for upper half added with implementation.

--- src/day_5.rs
+++ src/day_5.rs
@@ -3,7 +3,15 @@ fn seat_rows() -> Vec<u32> {
 }
 
 fn lower_half(seats: &[u32]) -> Vec<u32> {
-    (0..64).collect()
+    assert!(seats.len() % 2 == 0);
+    let mid_point = seats.len() / 2;
+    seats[0..mid_point].to_vec()
+}
+
+fn upper_half(seats: &[u32]) -> Vec<u32> {
+    assert!(seats.len() % 2 == 0);
+    let mid_point = seats.len() / 2;
+    seats[mid_point..seats.len()].to_vec()
 }
 
 #[cfg(test)]
@@ -14,5 +22,11 @@ mod test {
     fn test_lower_half() {
         let correct_list = (0..64).collect::<Vec<u32>>();
         assert_eq!(lower_half(&seat_rows()), correct_list)
+    }
+
+    #[test]
+    fn test_upper_half() {
+        let correct_list: Vec<u32> = (64..128).collect();
+        assert_eq!(upper_half(&seat_rows()), correct_list)
     }
 }
